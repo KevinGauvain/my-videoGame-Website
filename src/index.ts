d83e9183aaa4ba43916092a5fb50ad3eeb393a20
Kevin Gauvain <kevin.gauvain@decathlon.com>
2022-02-16 17:49:03 +0100

    update16Février

--- src/index.ts
+++ src/index.ts
@@ -27,12 +27,34 @@ app.get("/test", (req, response) => {
       throw error;
     }
     const result = JSON.parse(body);
+    //console.log("-------------lg30--------", result);
+    result.platforms;
+    //console.log("--------lg32--------", result.platforms);
+    // result.total;
+    // console.log("--------lg34---------", result.total);
+    const pushed = [];
+    for (let i = 0; i < result.platforms.length; i++) {
+      pushed.push(result.platforms[i].name);
+    }
+
+    response.render("home", { list: pushed, total: result.total });
+  });
+});
+
+/*
+app.get("/test", (req, response) => {
+  request("http://videogame-api.fly.dev/platforms", (error, body) => {
+    if (error) {
+      throw error;
+    }
+    const result = JSON.parse(body);
     result.platforms.name;
     result.total;
 
-    response.render("home", { liste: result.platforms[0].name, total: result.total });
+    response.render("home", { list: result.platforms[0].name, total: result.total });
   });
 });
+*/
 
 /*
 app.get("/random-joke", (req, response) => {
